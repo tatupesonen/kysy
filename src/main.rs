7e91fde418674b7d6fc8aff496b99cec411e43a8
Tatu Pesonen <tatu@narigon.dev>
2024-05-31 00:36:17 +0300

    fix: cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use owo_colors::OwoColorize;
 use serde::{Deserialize, Serialize};
 use std::{
-    io::{self, Read, Write},
+    io::Write,
     path::{Path, PathBuf},
 };
 
@@ -43,8 +43,7 @@ struct Request {
     context: Option<Vec<i64>>,
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let q = args.question.join(" ");
 
@@ -72,44 +71,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         std::fs::File::create(&last_ctx_path).expect("Unable to create context.json file.");
     }
 
-    let prompt = r#"Only answer to me in JSON. The format you should respond to me in is as follows: {"code": string, "description": string, "programming_language": string, "extension": string}. You can use the description field to give me some information about a command. You will be answering programming questions. Use the programming_language field to respond to what language the "code" output is in. Use the "extension" field to output the file extension for the code without the dot. Remember to handle newlines correctly in the JSON and escape any characters you need to. You should always answer the description in the same user as the language is asking the question in. This is the user's question: "#;
-    
+    let prompt = r#"Only answer to me in JSON. The format you should respond to me in is as follows: {"code": string, "description": string, "programming_language": string, "extension": string}. You can use the description field to give me some information about a command, this field should always be in the same language as the question the user is asking. You will be answering programming questions. Use the programming_language field to respond to what language the "code" output is in. Use the "extension" field to output the file extension for the code without the dot. Remember to handle newlines correctly in the JSON and escape any characters you need to. This is the user's question: "#;
 
     let mut file = String::new();
     if args.file.is_some() {
-      file = std::fs::read_to_string(args.file.unwrap()).unwrap();
+        file = std::fs::read_to_string(args.file.unwrap()).unwrap();
     }
 
     let prompt = format!("{prompt}, {} {}", q, file);
 
     let body: Response = ureq::post("http://localhost:11434/api/generate")
-      .send_json(Request {
-        model: "llama3".into(),
-        prompt: prompt,
-        stream: false,
-        context: ctx
-      })?
-      .into_json()?;
+        .send_json(Request {
+            model: "llama3".into(),
+            prompt,
+            stream: false,
+            context: ctx,
+        })?
+        .into_json()?;
 
-    let mut ctx_file = std::fs::OpenOptions::new().write(true).truncate(true).open(last_ctx_path).expect("Unable to open context file for writing.");
+    let mut ctx_file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(last_ctx_path)
+        .expect("Unable to open context file for writing.");
     let out_json = serde_json::to_string(&body.context).unwrap();
     write!(ctx_file, "{}", out_json).expect("Unable to write to context file");
 
-    let resp: ModelResponse = serde_json::from_str(&body.response).expect("Model responded with malformed JSON.");
+    let resp: ModelResponse =
+        serde_json::from_str(&body.response).expect("Model responded with malformed JSON.");
     println!("Question:\n  {}", q);
     println!("Description:\n  {}", resp.description.green());
-    if &resp.programming_language != "" {
-      println!("Language: {}", resp.programming_language.yellow());
+    if !resp.programming_language.is_empty() {
+        println!("Language: {}", resp.programming_language.yellow());
     }
-    if &resp.code != "" {
-      println!("Code:\n\n{}", resp.code.on_white().black());
+    if !resp.code.is_empty() {
+        println!("Code:\n\n{}", resp.code.on_white().black());
     }
 
     if args.save {
-      let ext = if &resp.extension == "" { ".txt" } else { &resp.extension};
-      let file_path = format!("./output.{}", ext);
-      std::fs::write(&file_path, &resp.code)?;
-      println!("Output saved to {file_path}");
+        let ext = if resp.extension.is_empty() {
+            ".txt"
+        } else {
+            &resp.extension
+        };
+        let file_path = format!("./output.{}", ext);
+        std::fs::write(&file_path, &resp.code)?;
+        println!("Output saved to {file_path}");
     }
 
     Ok(())
